0d2876fa7c168392ec7be1f8ad1ceeb9a51c514a
310310310310 <piku.p.p.p.p@ezweb.ne.jp>
2024-12-14 22:14:15 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,6 @@
 // 定数の宣言・値の代入
 let userHeight = 1.7 ;
 let userWeight = 68 ;
+let userBmi = (userWeight / (userHeight * userHeight)) ;
 // コンソールへの出力
-console.log(userWeight / (userHeight * userHeight)) ;
+console.log(userBmi) ;
